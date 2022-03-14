4435826d6312bf8863735631a3a6fc1436e690d8
Yuni Teh <yuni@u.northwestern.edu>
2022-03-14 13:44:16 -0500

    removed adabound optimizer

--- python/adapt/loop.py
+++ python/adapt/loop.py
@@ -36,11 +36,9 @@ def train_task(model, num_iter, disp_freq, x_train, y_train, x_test=[], y_test=N
         if lams[l] == 0:
             # optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
             optimizer = tf.keras.optimizers.SGD(learning_rate=0.000001)#,clipvalue=.5)
-            # optimizer = AdaBoundOptimizer(learning_rate=0.001, final_lr=0.01)
         else:
             # optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
             optimizer = tf.keras.optimizers.SGD(learning_rate=0.000001)#,clipvalue=.5)
-            # optimizer = AdaBoundOptimizer(learning_rate=0.0001, final_lr=0.001)
         
         # train functions
         train_ewc = get_train()
