316c7c57d42f7d72bc7bb0845dc503ad03bce8ce
Yuni Teh <yuni@u.northwestern.edu>
2022-04-05 00:06:05 -0500

    copied over from cbm and deleted comments, 0404 post cat 70

--- python/adapt/utils/data_utils.py
+++ python/adapt/utils/data_utils.py
@@ -782,3 +782,31 @@ def mahal(m1,s1,m2,s2):
         cov_inv = np.linalg.pinv((s1+s2)/2)
     m = np.sqrt((m1-m2).T*(cov_inv)*(m1-m2))
     return m
+
+def split_data(train_data,train_params,it=1):
+    tr_i = np.zeros((train_params.shape[0],))
+    te_i = np.zeros((train_params.shape[0],))
+    for cls in np.unique(train_params[:,-1]):
+        dof = np.array(np.where(train_params[:,-1] == cls))
+        if it == 0 or it == 1:
+            tr_i[dof[0,:dof.shape[1]//2]] = 1
+            te_i[dof[0,dof.shape[1]//2:]] = 1
+        elif it == 2 or it == 3:
+            tr_i[dof[0,:dof.shape[1]//4]] = 1
+            tr_i[dof[0,3*dof.shape[1]//4:]] = 1
+            te_i[dof[0,dof.shape[1]//4:3*dof.shape[1]//4]] = 1
+        elif it == 4:
+            tr_i[dof[0,::2]] = 1
+            te_i[dof[0,1::2]] = 1
+        
+    train_temp = train_data[tr_i.astype(bool),...]
+    params_temp = train_params[tr_i.astype(bool),...]
+    val_data = train_data[te_i.astype(bool),...]
+    val_params = train_params[te_i.astype(bool),...]
+
+    train_data, train_params = train_temp, params_temp
+
+    if it == 0 or it == 2 or it == 4:
+        return train_data, train_params, val_data, val_params
+    elif it == 1 or it == 3:
+        return val_data, val_params,train_data, train_params
